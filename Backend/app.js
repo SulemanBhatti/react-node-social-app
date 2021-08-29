36e99e1bf158021fb0feb9610d046c81040af4d8
sulemanbhatti <sulemanbhatti.business@gmail.com>
2021-08-29 23:38:02 +0500

    Setting up a Post model for new posts

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const feedRoutes = require('./routes/feed');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json()); //application/json
+const MONGODB_URI = 'mongodb://ahmad1:p2K6S5mmpktG4KfI@cluster0-shard-00-00.xmre6.mongodb.net:27017,cluster0-shard-00-01.xmre6.mongodb.net:27017,cluster0-shard-00-02.xmre6.mongodb.net:27017/messages?ssl=true&replicaSet=atlas-9tacbu-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,5 +14,11 @@ app.use((req, res, next)=>{
 });
 
 app.use('/feed', feedRoutes);
+
+mongoose.connect(MONGODB_URI)
+.then(result=>{
+    app.listen(8080);
+})
+.catch(err=>console.log(err));
 
 app.listen(8080);
