d94145b3a83dc4c32d6e4be8fea706b2121e06f4
sulemanbhatti <sulemanbhatti.business@gmail.com>
2021-08-30 19:08:35 +0500

    Get posts from db and show them to client also select post by Id

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -2,19 +2,15 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) =>{
-    res.status(200).json({
-        posts: [
-          {     
-              _id: '1',
-              title: 'First Post', 
-              content: 'This is first post', 
-              imageUrl: 'images/picture.png',
-              creator: {
-                  name: 'Suleman'
-              },
-              createdAt: new Date()
-          }
-        ]
+    Post.find()
+    .then(posts => {
+        res.status(200).json({message: 'Fetched posts successfully.', posts: posts})
+    })
+    .catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
     });
 };
 
@@ -47,3 +43,23 @@ exports.createPost = (req, res, next) => {
     })
     .catch(err=>console.log(err));
 }
+
+exports.getPost = (req, res, next) =>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if(!post){
+            const error = new Error('Could not found any post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({message: 'Post fetched.', post: post});
+    })
+    .catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
+        
