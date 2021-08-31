9523d887d06a8e8d7ee947190e46c84d932fbde8
sulemanbhatti <sulemanbhatti.business@gmail.com>
2021-08-31 23:19:11 +0500

    Image upload and validation, few tweaks left for this task

--- Backend/routes/feed.js
+++ Backend/routes/feed.js
@@ -10,11 +10,16 @@ router.get('/posts', feedController.getPosts);
 
 // POST /feed/posts
 
+router.get('/post/:postId', feedController.getPost);
+
 router.post('/post', [
     body('title').trim().isLength({min:5}),
     body('content').trim().isLength({min:5})
 ], feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost);
+router.put('/post/:postId',[
+    body('title').trim().isLength({min:5}),
+    body('content').trim().isLength({min:5})
+], feedController.updatePost);
 
 module.exports = router;
