9ceb292fc3dbc15e03a1dd958de017888e167f1d
sulemanbhatti <sulemanbhatti.business@gmail.com>
2021-09-06 09:24:15 +0500

    Added user signup validation but password encryption left

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,7 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const MONGODB_URI = 'mongodb://ahmad1:p2K6S5mmpktG4KfI@cluster0-shard-00-00.xmre6.mongodb.net:27017,cluster0-shard-00-01.xmre6.mongodb.net:27017,cluster0-shard-00-02.xmre6.mongodb.net:27017/messages?ssl=true&replicaSet=atlas-9tacbu-shard-0&authSource=admin&retryWrites=true&w=majority';
 
+// Setting images folder destination
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images');
@@ -20,6 +21,8 @@ const fileStorage = multer.diskStorage({
   }
 });
 
+
+// Filtering images format and allowing only specific
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/png' ||
@@ -33,11 +36,14 @@ const fileFilter = (req, file, cb) => {
 };
 
 app.use(bodyParser.json()); // application/json
+
+// storing images in images folder uploaded by User
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+// App headers for authorization
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -48,16 +54,20 @@ app.use((req, res, next) => {
   next();
 });
 
+// App routes
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
+// Error handling
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
+// Database connection
 mongoose.connect(MONGODB_URI)
 .then(result=>{
     app.listen(8080);
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,32 @@
 const express = require('express');
-
+const { body } = require('express-validator');
+const User = require('../models/user');
 const router = express.Router();
+const authController = require('../controllers/auth');
 
-router.put('/signup');
+router.put('/signup', [
+    // Validating user email
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .custom((value, { req }) => {
+        return User.findOne({ email: value})
+        .then(userDoc=>{
+            if(userDoc){
+                return Promise.reject('Email address already exists!');
+            }
+        })
+    })
+    .normalizeEmail(),
+    // Validating user password
+    body('pasword')
+    .trim()
+    .isLength({min: 5}),
+    // Validating user name
+    body('name')
+    .trim()
+    .not()
+    .isEmpty()
+], authController.signUp);
 
 module.exports = router;
